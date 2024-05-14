354c52b4f51621c5340f9a8e1c171fd00f22e46c
EOEmmanuel1 <166291941+EOEmmanuel1@users.noreply.github.com>
2024-05-14 02:14:13 -0700

    Add files via upload

---
+++ Index.js
@@ -0,0 +1,46 @@
+!
+
+
+
+//console.log(355667)
+
+//const moon = "35"
+//const accountName= "emmanue Obuh"
+
+//console.log(moon)
+
+//console.log(accountName)
+//const aas = 3
+//const abs = 4
+//const answ = aas + abs
+//console.log (answ)
+
+
+//const randomNumber = math.random()+100
+//document.write (randomNumber)
+
+
+const students=[
+    {
+        firstName:"Emmanuel",
+        surName:"Obuh",
+        score:90
+    } ,
+    {
+        firstName:"John",
+        surName:"ObuhDavid",
+        score:50
+    },
+    
+    {
+        firstName:"Sunday",
+        surName:"Deborah",
+        score:80
+    }
+]
+
+    
+
+
+
+console.log(students)
